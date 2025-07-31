53e345f57bc539a021da156fb3db11b0be63e4d5
Jk Devloper <141899550+sunil143786@users.noreply.github.com>
2025-07-31 11:40:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyrogram, os, asyncio
 
-try: app_id = int(os.environ.get("app_id", None))
+try: api_id = int(os.environ.get("api_id", None))
 except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
 try: api_hash = os.environ.get("api_hash", None)
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
